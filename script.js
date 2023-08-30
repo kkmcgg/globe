d34f5a1c791a9469ec14ec48496cf8817d5e2c33
Kevin Mcguigan <36888812+kkmcgg@users.noreply.github.com>
2023-08-30 13:30:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Initialize the scene, camera, and renderer
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true })
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById('container').appendChild(renderer.domElement);
 
